3fb84e26c765ac3473197a36633431d2e0edc0e9
GeorgeBeta <jorge@zaporta.com>
2020-01-22 18:50:33 +0100

    Hasta punto 108 - antes de JWT

---
+++ server/models/usuario.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+}
+
+let Schema = mongoose.Schema;
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El campo es nombre es necesario']
+    },
+    email: {
+        type: String,
+        required: [true, 'El campo email es necesario'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es imprescindible']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userobject = user.toObject();
+    delete userobject.password;
+    return userobject;
+};
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,39 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 require('./config/config');
 
+app.use(require('./routes/usuario'));
+
 app.get('/', function(req, res) {
     res.json('Hola mundo');
 });
 
-app.get('/usuario', function(req, res) {
-    res.json('getUsuario');
-});
-
-app.post('/usuario/', function(req, res) {
-    let body = req.body;
-    if (body.name === undefined) {
-        res.status(400).json({
-            Ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            user: req.body
-        });
+mongoose.connect(
+    'mongodb+srv://georgebeta:basededatos470998@clustergeorgebeta-yuhgi.mongodb.net/cafe', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log('Base de Datos ON-LINE');
     }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    res.send(req.params.id);
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('deleteUsuario');
-});
+);
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando por el puerto', process.env.PORT);
